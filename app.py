21e5073a08caca46bcdc85071771f40258cb0b4f
Osvaldo Valdivia <ovaldovia983@gmail.com>
2025-10-22 17:53:38 -0600

    Ajusta De Cards, Rutas y Enlaces

--- app.py
+++ app.py
@@ -6,5 +6,29 @@ app = Flask(__name__)
 def index(): 
     return render_template('index.html')
 
+@app.route("/condicionales")
+def condicionales():
+    return 'Condicionales'
+
+@app.route("/ciclos")
+def ciclos():
+    return 'Ciclos'
+
+@app.route("/arrays")
+def arrays():
+    return 'Arrays'
+
+@app.route("/matrinces")
+def matrices():
+    return 'Matrices'
+
+@app.route("/funciones")
+def funciones():
+    return 'Funciones'
+
+@app.route("/strings")
+def strings():
+    return 'Strings'
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
