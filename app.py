ddeb763e09d6fa7c2391249007a606f9845916d6
Osvaldo Valdivia <ovaldovia983@gmail.com>
2026-04-02 20:27:38 -0600

    Crecion Macro Carrusel Para Ejercicios

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
 import yaml
+from pathlib import Path
 
 app = Flask(__name__)
 
-def load_data(filename):
-    with open(f'data/{filename}/{filename}.yml', encoding='utf-8') as f:
+EXERCISES_DIR = Path('data/')
+
+def load_data(seccion, filename):
+    with open(EXERCISES_DIR / seccion / filename, encoding='utf-8') as f:
         return yaml.safe_load(f)
+    
+def get_all_exercises(seccion):
+    files = ["entender.yml", "diagnosticar.yml", "construir.yml"]
+    all_exercises = []
+
+    for file in files:
+        data = load_data(seccion, file)
+        all_exercises.extend(data)
+
+    return all_exercises
 
 @app.route("/")
 def index(): 
@@ -13,7 +26,9 @@ def index():
 
 @app.route("/condicionales")
 def condicionales():
-    return render_template('condicionales.html', topics=load_data('condicionales'))
+    return render_template('condicionales.html',
+                           topics=load_data('condicionales', 'condicionales.yml'),
+                           exercises=get_all_exercises('condicionales'))
 
 @app.route("/ciclos")
 def ciclos():
