f895457030318d3ade283f9410eff12e494b7bc5
Osvaldo Valdivia <ovaldovia983@gmail.com>
2026-04-03 18:15:45 -0600

    Mover Slide Y Verificacion Cantidad Ejercicios

--- static/JS/carrusel2.js
+++ static/JS/carrusel2.js
@@ -1,11 +1,12 @@
 const progressCurrent = document.querySelector('.exercise-carousel__current');
 const progressTotal = document.querySelector('.exercise-carousel__total');
 const progressFill = document.querySelector('.exercise-carousel__progress-fill');
-const slideContainer = document.querySelector('.exercise-carousel__slide');
+const slideContainer = document.querySelector('.exercise-carousel');
 const btnPrev = document.querySelector('.exercise-carousel__button--prev');
 const btnNext = document.querySelector('.exercise-carousel__button--next');
 
 let ejercicioActual = 1
+const CANTIDAD_EJERCICIOS = 10
 
 function verificarRespuesta() {
     const slideActivo = document.querySelector('.exercise-carousel__slide--active');
@@ -22,7 +23,7 @@ function verificarRespuesta() {
 
     const respuestaUsuario = radioSeleccionado.value;
 
-    const feedback = document.querySelector('.exercise-carousel__feedback')
+    const feedback = slideActivo.querySelector('.exercise-carousel__feedback')
     if(respuestaUsuario === slideActivo.dataset.respuesta){
         feedback.querySelector('p:last-of-type').style.display = 'none'
         feedback.querySelector('p:first-of-type').style.display = 'block'
@@ -33,8 +34,45 @@ function verificarRespuesta() {
     }
 }
 
+function moverSlide(sentido){
+    const slideActivo = document.querySelector('.exercise-carousel__slide--active');
+    const slideProximo = sentido === 'next'? slideActivo.nextElementSibling : slideActivo.previousElementSibling
+    slideActivo.classList.add('exercise-carousel__slide--exit')
+
+    setTimeout(function(){
+        slideActivo.classList.remove('exercise-carousel__slide--active', 'exercise-carousel__slide--exit')
+    }, 500)
+
+    setTimeout(function(){
+        slideProximo.classList.add('exercise-carousel__slide--active')
+    }, 500)
+}
+
+function verificarEjercicioActual(){
+    if(ejercicioActual >= CANTIDAD_EJERCICIOS)
+        btnNext.disabled = true
+    else if(ejercicioActual == 1)
+        btnPrev.disabled = true
+    else{
+        btnNext.disabled = false
+        btnPrev.disabled = false
+    }
+}
+
 slideContainer.addEventListener('click', (e) => {
     if (e.target.classList.contains('exercise-carousel__verify')) {
         verificarRespuesta();
     }
 });
+
+btnNext.addEventListener('click', function(){
+    ejercicioActual++
+    verificarEjercicioActual()
+    moverSlide('next') 
+})
+
+btnPrev.addEventListener('click', function(){
+    ejercicioActual--
+    verificarEjercicioActual()
+    moverSlide('prev') 
+})
